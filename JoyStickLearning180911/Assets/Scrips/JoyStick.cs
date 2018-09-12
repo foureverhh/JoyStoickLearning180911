1e0ad201c8a42f53ba03f236060fb25c567042b7
foureverhh <foureverhh@gmail.com>
2018-09-12 10:09:22 +0200

    change operations not on camera layer but on game layer, it still works

--- JoyStickLearning180911/Assets/Scrips/JoyStick.cs
+++ JoyStickLearning180911/Assets/Scrips/JoyStick.cs
@@ -10,29 +10,48 @@ public class JoyStick : MonoBehaviour {
     private Vector2 pointA;
     private Vector2 pointB;
     public Transform circle;
-   
 
-	// Use this for initialization
-	void Start () {
-		
+    //let joystick have original place
+    private Vector2 originPoint;
+    //calculate the offset vector2 between mouse click and joystick original place
+    private Vector2 offsetToOri;
+    //private Vector2 originPointCamera;
+
+    // Use this for initialization
+    void Start () {
+        originPoint = circle.position;
+        //originPointCamera = Camera.main.ScreenToWorldPoint(new Vector3(originPoint.x, originPoint.y, Camera.main.transform.position.z));
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.transform.position.z));
+            pointA = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));//Camera.main.transform.position.z));
             circle.transform.position = pointA;
+            //Calculate the offest of pointA and orginPointCamera
+            //offsetToOri = pointA - originPointCamera;
+            //Make circle stay at originPoint
+            //circle.transform.position =  offsetToOri;
         }
 
         if (Input.GetMouseButton(0))
         {
             touchStart = true;
-            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            pointB = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));// Camera.main.transform.position.z));
         }
-        else
+        //else
+        //{
+        //    touchStart = false;
+        //}
+        //Let joystick be back to original place
+        if (Input.GetMouseButtonUp(0))
         {
             touchStart = false;
+            circle.position = originPoint;
         }
 	}
     private void FixedUpdate()

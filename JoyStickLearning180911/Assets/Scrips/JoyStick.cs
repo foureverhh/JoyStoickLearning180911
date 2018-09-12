68346953c9ea4b8b4ff0d5af9db7285262a1df17
foureverhh <foureverhh@gmail.com>
2018-09-12 09:21:40 +0200

    joystick works but can not fixed in a place

---
+++ JoyStickLearning180911/Assets/Scrips/JoyStick.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour {
+
+    public Transform player;
+    public float speed = 5.0f;
+    private bool touchStart = false;
+    private Vector2 pointA;
+    private Vector2 pointB;
+    public Transform circle;
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.transform.position.z));
+            circle.transform.position = pointA;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            touchStart = false;
+        }
+	}
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = pointB - pointA;
+            Vector2 directionWithRadius = Vector2.ClampMagnitude(offset, 1.0f);
+            //camera and GUI view are oppsite ??
+            MoveCharacter(directionWithRadius);
+            circle.transform.position = new Vector2(pointA.x + directionWithRadius.x, pointA.y + directionWithRadius.y);
+        }
+    }
+
+    void MoveCharacter(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
